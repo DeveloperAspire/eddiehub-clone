87fba81a5073fa5668915fe5877cbccb7c20f820
Franklin <franklinokolie86@gmail.com>
2021-01-18 14:31:20 +0100

    Made customized colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors:{
+      "darkblue": "#1A202C",
+      "whiteblue": "#EBF4FE",
+      "white": "#ffff",
+      "green": "#53BFD0",
+      "litgreen": "#68D391",
+      "blue": "#2C6CB0",
+      "litblue": "#4299E1",
+      "yellow": "#F6E05E",
+      "purple": "#B794F4"
+
+
+
+    },
     extend: {},
   },
   variants: {
